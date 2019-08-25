d1c384aa4ea1db6e023c231fecc1ec32e5c90225
Abe <abe@humboldt.edu>
2019-08-24 19:22:13 -0700

    created a game plan

---
+++ script.js
@@ -0,0 +1,18 @@
+/* Gameplan:
+  1.create an n by n array
+  2.consider four states:
+    a.able to go right: (square to the right is set to zero and not undefined)
+    b.able to go down: down is not zero or undefined
+    c.able to go left: left is not zero or undefined
+    d.able to go up: up is not zero or undefined
+  3.set initial state to "Go right"
+  4.initalize x,y at 0,0
+  5.for i -> n^2
+    a.if current state is "go right"
+        set current cell equal to n
+        check if we are still able to do so
+          if not: set next state to "Go down"
+    b.rinse and repeat for go down go left and go up
+      they should be occuring cyclically.
+
+*/
